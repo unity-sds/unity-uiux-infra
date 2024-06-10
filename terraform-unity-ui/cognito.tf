f2724650896de281b2641c8daf7c605b2c34fb6b
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-06-10 11:12:00 -0700

    Removed unused variables file.

--- terraform-unity-ui/cognito.tf
+++ terraform-unity-ui/cognito.tf
@@ -11,13 +11,13 @@ resource "aws_cognito_user_pool_client" "userpool_client" {
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_scopes = ["email", "openid", "profile"]
   auth_session_validity = 3
-  callback_urls = "${data.aws_cloudfront_distribution.cloudfront_distribution.domain_name}/dashboard"
+  callback_urls = ["https://www.dev.mdps.mcp.nasa.gov:4443/${var.project}/${var.venue}/dashboard"]
   enable_token_revocation = true
   explicit_auth_flows = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
   generate_secret = false
   id_token_validity = 60
-  logout_urls = ""    // todo: determine if logout urls are needed
-  prevent_user_existence_errors = true
+  logout_urls = []    // todo: determine if logout urls are needed
+  prevent_user_existence_errors = "ENABLED"
   refresh_token_validity = 30
   supported_identity_providers = ["COGNITO"]
 
