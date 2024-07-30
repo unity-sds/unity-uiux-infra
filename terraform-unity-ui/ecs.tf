b09b24086b0fa460b2f892a26c7ae70101f6b554
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-07-30 09:29:39 -0700

    Updated environment variables that are injected when starting application container.

--- terraform-unity-ui/ecs.tf
+++ terraform-unity-ui/ecs.tf
@@ -16,6 +16,14 @@ resource "aws_ecs_task_definition" "app" {
         image = var.app_image
         environment = [
           {
+            name = "ENV_UNITY_UI_BASE_URL"
+            value = "/${var.project}/${var.venue}/dashboard"
+          },
+          {
+            name = "ENV_UNITY_UI_HEALTH_API_ENDPOINT"
+            value = "https://www.${data.aws_ssm_parameter.shared_services_domain.value}:4443/${var.project}/${var.venue}/management/api/health_checks"
+          },
+          {
             name = "ENV_UNITY_UI_AUTH_OAUTH_CLIENT_ID"
             value = var.cognito_client_id
           },
